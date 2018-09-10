095548b18aca8e8758442ac0e78041a9a6e68ba4
Gregory Kallenberg <gregorykallenberg@nypl.org>
2018-09-10 11:49:43 -0400

    add debug for preprocess of ncip response

--- src/NCIPClient.php
+++ src/NCIPClient.php
@@ -141,6 +141,8 @@ class NCIPClient
             ]
         );
 
+        APILogger::addDebug('ncipResponse before parse', [$response->getBody()]);
+
         self::setResponse(new \SimpleXMLElement((string)$response->getBody()));
 
         APILogger::addDebug('ncipResponse', [self::getResponse()]);
