d29150f17e281686ee3075c1912aa91c5b148791
Keith Bauer <bauer.keith@gmail.com>
2024-09-30 14:40:26 -0500

    Update routes

--- index.php
+++ index.php
@@ -3,25 +3,50 @@ require __DIR__ . '/vendor/autoload.php';
 
 use NYPL\Services\Controller\CheckinRequestController;
 use NYPL\Services\ServiceContainer;
-use NYPL\Services\Swagger;
+use NYPL\Starter\SwaggerGenerator;
 use NYPL\Starter\Service;
 use NYPL\Starter\Config;
 use NYPL\Starter\ErrorHandler;
+use Aura\Di\Injection\InjectionFactory;
+use Aura\Di\Resolver\Resolver;
+use Aura\Di\ContainerBuilder;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 try {
     Config::initialize(__DIR__);
 
-    $container = new ServiceContainer();
+    $service = new Service();
+    $service->addBodyParsingMiddleware();
 
-    $service = new Service($container);
+    $service->get("/docs/checkin-requests", function (Request $request, Response $response) {
+        return SwaggerGenerator::generate(
+            [__DIR__ . "/src", __DIR__ . "/vendor/nypl/microservice-starter/src"],
+            $response
+        );
+    });
 
-    $service->get('/docs/checkin-requests', Swagger::class);
+    $service->get("/docs/checkin-requests-sync", function (Request $request, Response $response) {
+        return SwaggerGenerator::generate(
+            [__DIR__ . "/src", __DIR__ . "/vendor/nypl/microservice-starter/src"],
+            $response
+        );
+    });
 
-    $service->get('/docs/checkin-requests-sync', Swagger::class);
+    $service->post("/api/v0.1/checkin-requests", function (Request $request, Response $response) {
+        $this->set("response", $response);
+        $this->set("request", $request);
+        $controller = new CheckinRequestController($this);
+        return $controller->createCheckinRequest();
+    });
 
-    $service->post('/api/v0.1/checkin-requests', CheckinRequestController::class . ':createCheckinRequest');
-
-    $service->post('/api/v0.1/checkin-requests-sync', CheckinRequestController::class . ':createCheckinRequest');
+    $service->post("/api/v0.1/checkin-requests-sync", function (Request $request, Response $response) {
+        $this->set("response", $response);
+        $this->set("request", $request);
+        $controller = new CheckinRequestController($this);
+        return $controller->createCheckinRequest();
+    });
 
     $service->run();
 } catch (Exception $exception) {
